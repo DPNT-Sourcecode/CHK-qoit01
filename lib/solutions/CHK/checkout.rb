648111654336a89230c26f7ac9963c5beb403023
sbx_user1051 <sbx_user1051@169.254.15.85>
2023-08-24 20:31:29 +0000

    Thu Aug 24 20:31:29 UTC 2023

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -6,16 +6,16 @@ class Checkout
   #   +------+-------+------------------------+
   # | A    | 50    | 3A for 130, 5A for 200 | x
   # | B    | 30    | 2B for 45              | x
-  # | E    | 40    | 2E get one B free      |
+  # | E    | 40    | 2E get one B free      | x
   # | F    | 10    | 2F get one F free      | x
   # | H    | 10    | 5H for 45, 10H for 80  | x
   # | K    | 80    | 2K for 150             | x
-  # | N    | 40    | 3N get one M free      |
+  # | N    | 40    | 3N get one M free      | x
   # | P    | 50    | 5P for 200             | x
   # | Q    | 30    | 3Q for 80              | x
   # | R    | 50    | 3R get one Q free      |
-  # | U    | 40    | 3U get one U free      |
-  # | V    | 50    | 2V for 90, 3V for 130  |
+  # | U    | 40    | 3U get one U free      | x
+  # | V    | 50    | 2V for 90, 3V for 130  | x
   # +------+-------+------------------------+
 
   @price_lookup = {
@@ -27,28 +27,42 @@ class Checkout
   }
 
   @discounts = [
-    {items: ["A" => 5], discount: 50},
-    {items: ["A" => 3], discount: 20},
-    {items: ["B" => 2], discount: 15},
-    {items: ["F" => 3], discount: 10},
-    {items: ["H" => 10], discount: 20},
-    {items: ["H" => 5], discount: 5},
-    {items: ["K" => 2], discount: 10},
-    {items: ["P" => 5], discount: 50},
-    {items: ["Q" => 3], discount: 10},
-    {items: ["U" => 4], discount: 40},
-    {items: ["V" => 3], discount: 130},
-    {items: ["V" => 2], discount: 50},
+    { items: ["E" => 2, "B" => 1], discount: 30 },
+    { items: ["N" => 3, "M" => 1], discount: 15 },
+    { items: ["R" => 3, "Q" => 1], discount: 30 },
+
+    { items: ["A" => 5], discount: 50 },
+    { items: ["A" => 3], discount: 20 },
+    { items: ["B" => 2], discount: 15 },
+    { items: ["F" => 3], discount: 10 },
+    { items: ["H" => 10], discount: 20 },
+    { items: ["H" => 5], discount: 5 },
+    { items: ["K" => 2], discount: 10 },
+    { items: ["P" => 5], discount: 50 },
+    { items: ["Q" => 3], discount: 10 },
+    { items: ["U" => 4], discount: 40 },
+    { items: ["V" => 3], discount: 130 },
+    { items: ["V" => 2], discount: 50 },
   ]
 
-
   class << self
-    attr_accessor :price_lookup
+    attr_accessor :price_lookup, :discounts
   end
 
   def basket_discounts(sku_hash)
     sku_hash = sku_hash.clone
     total_discount = 0
+
+    self.class.discounts.map do |discount|
+      while discount[:items].all? { |sku, required_quantity| sku_hash[:sku] >= required_quantity }
+
+        total_discount += discount[:discount]
+
+      end
+
+
+    end
+
 
     if sku_hash["E"] && sku_hash["B"]
       max_b_to_remove = sku_hash["E"] / 2
@@ -56,7 +70,6 @@ class Checkout
       total_discount += b_to_remove * 30
       sku_hash["B"] -= b_to_remove
     end
- 
 
     total_discount
   end
@@ -74,6 +87,7 @@ class Checkout
     end
   end
 end
+
 
 
 
