c83cbfd3278fe7e0388a7dd2a1d9c2838f496bea
sbx_user1051 <sbx_user1051@169.254.15.85>
2023-08-24 19:25:27 +0000

    Thu Aug 24 19:25:27 UTC 2023

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -1,8 +1,30 @@
 # noinspection RubyUnusedLocalVariable
 class Checkout
 
+  # +------+-------+----------------+
+  # | Item | Price | Special offers |
+  # +------+-------+----------------+
+  # | A    | 50    | 3A for 130     |
+  # | B    | 30    | 2B for 45      |
+  # | C    | 20    |                |
+  # | D    | 15    |                |
+  # +------+-------+----------------+
+
+  price_lookup = {
+    "A": 50,
+    "B": 30,
+    "C": 20,
+    "D": 15
+  }
+
+  def price_for_item_quantity(item, quantity)
+    {}
+
+  end
+
   def checkout(skus)
     raise 'Not implemented'
   end
 
 end
+
