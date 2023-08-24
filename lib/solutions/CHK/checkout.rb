2e013cb93ec9b4cbf83abf88172a3d9b31a1dccb
sbx_user1051 <sbx_user1051@169.254.15.85>
2023-08-24 20:25:29 +0000

    Thu Aug 24 20:25:29 UTC 2023

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -4,19 +4,32 @@ class Checkout
   # +------+-------+------------------------+
   # | Item | Price | Special offers         |
   #   +------+-------+------------------------+
-  # | A    | 50    | 3A for 130, 5A for 200 | x
-  # | B    | 30    | 2B for 45              | x
-  # | E    | 40    | 2E get one B free      | x
-  # | F    | 10    | 2F get one F free      | x
+  # | A    | 50    | 3A for 130, 5A for 200 |
+  # | B    | 30    | 2B for 45              |
+  # | C    | 20    |                        |
+  # | D    | 15    |                        |
+  # | E    | 40    | 2E get one B free      |
+  # | F    | 10    | 2F get one F free      |
+  # | G    | 20    |                        |
   # | H    | 10    | 5H for 45, 10H for 80  |
+  # | I    | 35    |                        |
+  # | J    | 60    |                        |
   # | K    | 80    | 2K for 150             |
+  # | L    | 90    |                        |
+  # | M    | 15    |                        |
   # | N    | 40    | 3N get one M free      |
+  # | O    | 10    |                        |
   # | P    | 50    | 5P for 200             |
   # | Q    | 30    | 3Q for 80              |
   # | R    | 50    | 3R get one Q free      |
+  # | S    | 30    |                        |
+  # | T    | 20    |                        |
   # | U    | 40    | 3U get one U free      |
   # | V    | 50    | 2V for 90, 3V for 130  |
-
+  # | W    | 20    |                        |
+  # | X    | 90    |                        |
+  # | Y    | 10    |                        |
+  # | Z    | 50    |                        |
   # +------+-------+------------------------+
 
   @price_lookup = {
@@ -26,6 +39,12 @@ class Checkout
     "S" => 30, "T" => 20, "U" => 40, "V" => 50, "W" => 20, "X" => 90,
     "Y" => 10, "Z" => 50
   }
+
+  @discounts = [
+    {}
+
+  ]
+
 
   class << self
     attr_accessor :price_lookup
@@ -48,7 +67,11 @@ class Checkout
     total_discount += 10 * (sku_hash["F"] / 3) if sku_hash["F"]
     total_discount += 20 * (sku_hash["H"] / 10) if sku_hash["H"]
     total_discount += 5 * ((sku_hash["H"] % 10 ) / 5) if sku_hash["H"]
-    # puts total_discount
+    total_discount += 10 * (sku_hash["H"] / 2) if sku_hash["K"]
+    total_discount += 50 * (sku_hash["P"] / 5) if sku_hash["P"]
+    total_discount += 10 * (sku_hash["P"] / 3) if sku_hash["Q"]
+    total_discount += 20 * (sku_hash["V"] / 3) if sku_hash["V"]
+    total_discount += 10 * ((sku_hash["V"] % 3 ) / 2) if sku_hash["V"]
 
     total_discount
   end
@@ -66,6 +89,7 @@ class Checkout
     end
   end
 end
+
 
 
 
