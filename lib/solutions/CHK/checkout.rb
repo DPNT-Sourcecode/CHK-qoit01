359ef960e159dfed6d4d5be17796445e1dca1f24
sbx_user1051 <sbx_user1051@169.254.15.85>
2023-08-24 20:22:29 +0000

    Thu Aug 24 20:22:29 UTC 2023

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -1,13 +1,30 @@
 # noinspection RubyUnusedLocalVariable
 class Checkout
 
+  # +------+-------+------------------------+
+  # | Item | Price | Special offers         |
+  #   +------+-------+------------------------+
+  # | A    | 50    | 3A for 130, 5A for 200 | x
+  # | B    | 30    | 2B for 45              | x
+  # | E    | 40    | 2E get one B free      | x
+  # | F    | 10    | 2F get one F free      | x
+  # | H    | 10    | 5H for 45, 10H for 80  |
+  # | K    | 80    | 2K for 150             |
+  # | N    | 40    | 3N get one M free      |
+  # | P    | 50    | 5P for 200             |
+  # | Q    | 30    | 3Q for 80              |
+  # | R    | 50    | 3R get one Q free      |
+  # | U    | 40    | 3U get one U free      |
+  # | V    | 50    | 2V for 90, 3V for 130  |
+
+  # +------+-------+------------------------+
+
   @price_lookup = {
-    "A" => 50,
-    "B" => 30,
-    "C" => 20,
-    "D" => 15,
-    "E" => 40,
-    "F" => 10
+    "A" => 50, "B" => 30, "C" => 20, "D" => 15, "E" => 40, "F" => 10,
+    "G" => 20, "H" => 10, "I" => 35, "J" => 60, "K" => 80, "L" => 90,
+    "M" => 15, "N" => 40, "O" => 10, "P" => 50, "Q" => 30, "R" => 50,
+    "S" => 30, "T" => 20, "U" => 40, "V" => 50, "W" => 20, "X" => 90,
+    "Y" => 10, "Z" => 50
   }
 
   class << self
@@ -29,7 +46,8 @@ class Checkout
     total_discount += 20 * ((sku_hash["A"] % 5 ) / 3) if sku_hash["A"]
     total_discount += 15 * (sku_hash["B"] / 2) if sku_hash["B"]
     total_discount += 10 * (sku_hash["F"] / 3) if sku_hash["F"]
-
+    total_discount += 20 * (sku_hash["H"] / 10) if sku_hash["H"]
+    total_discount += 5 * ((sku_hash["H"] % 10 ) / 5) if sku_hash["H"]
     # puts total_discount
 
     total_discount
@@ -48,6 +66,7 @@ class Checkout
     end
   end
 end
+
 
 
 
